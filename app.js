ba76563498a95d9fae6c9c56cddd47a76391cd58
Ethan Fine <ethanfine@umass.edu>
2016-02-27 18:05:52 -0500

    validates jwts (and does nothing with the info)

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var njwt = require('njwt');
 
 var loginRoute = require('./routes/login');
 var userRoute = require('./routes/user');
 
 var app = express();
 
+//TODO read from config file
+var signingKey = "PLACEHOLDER";
+
 app.use(logger('dev'));
 app.use(bodyParser.json({strict: false}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//validate JWTs here
+app.use(function(req,res,next) {
+  //TODO are cookies where the token is?
+  var token = req.cookies.token;
+  nJwt.verify(token,signingKey,function(err,ver){
+    if(err){
+      next(err);
+    }else{
+      if(ver.isExpired()){
+        req.expired = true;
+      } else{
+        req.user = ver.body;
+      }
+      next();
+    }
+  });
+});
 
 app.use('/api/login', loginRoute);
 app.use('/api/user', userRoute);
