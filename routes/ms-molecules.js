2ea15d1a233004e894a3ec076d2613d8581ac945
Arif Topcu <09.arif@gmail.com>
2016-04-12 21:23:24 -0400

    Fixed molecules gateway. Added file uploading using http-proxy module

--- routes/ms-molecules.js
+++ routes/ms-molecules.js
@@ -2,16 +2,45 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var errorHandler = require('../lib/errorHandler.js');
+var proxyServer = require('http-proxy');
 
 var MS_MOLECULES_URL = "http://msmolecules:3000";
 
-// login and recieve authentication token
-router.post('/', function(req, res, next) {
+var proxy = proxyServer.createProxyServer({
+	host: MS_MOLECULES_URL
+});
+
+router.get('/', function(req, res, next) {
 	var reqParams = {
 		url: MS_MOLECULES_URL,
-		method:'GET',
-		json: true,
-		body: req.body
+		method:'GET'
+	}
+
+	request(reqParams, function (error, response, body) {
+		if(body.error){
+			next(errorHandler(body));
+			return;
+		}
+		res.send(body);
+	});
+});
+
+router.post('/upload', function(req, res, next) {
+	proxy.web(req, res, {
+		target: MS_MOLECULES_URL
+	});
+});
+
+router.get('/files', function(req, res, next) {
+	proxy.web(req, res, {
+		target: MS_MOLECULES_URL
+	});
+});
+
+router.get('/:name', function(req, res, next) {
+	var reqParams = {
+		url: MS_MOLECULES_URL + '/' + req.params.name,
+		method:'GET'
 	}
 
 	request(reqParams, function (error, response, body) {
